e1c4f4fd244da5dfd3c7018fc9a6af82f4e420df
Jan <jan.willems94@web.de>
2026-07-18 13:45:57 +0200

    change test size

--- code/classes/Config.py
+++ code/classes/Config.py
@@ -6,7 +6,7 @@ class Config:
     model_alias: str
     model_path: str
     n_train: int = 128
-    n_test: int = 100
+    n_test: int = 572
     n_val: int = 32
     seed: int = 42
     four_bit_quantization: bool = False
